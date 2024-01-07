abff5dbaacb4c57f1eb088d750481158073fcc7f
Zahid <55988827+Zarhad-01@users.noreply.github.com>
2024-01-07 20:35:10 +0000

    Update apiKeys.js

--- src/apiKeys.js
+++ src/apiKeys.js
@@ -1,11 +1,11 @@
 export const geoApiOptions = {
 	method: 'GET',
 	headers: {
-		'X-RapidAPI-Key': 'PLACE API KEY HERE',
+		'X-RapidAPI-Key': '607f67f2e7msh04ded8a33af3154p15b786jsn17fafdfc5ead',
 		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
 	}
   };
   export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo/adminDivisions";
   
   export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
-  export const WEATHER_API_KEY = "PLACE API KEY HERE";
+  export const WEATHER_API_KEY = "9c2e40aa0304ac04c5aedd3ad2bb8c58";
