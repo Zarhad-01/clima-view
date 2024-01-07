9ca458e579d4d297a3e80ffa8624221e77dd65cf
Zahid <55988827+Zarhad-01@users.noreply.github.com>
2024-01-07 20:50:26 +0000

    Updated Splash with name

--- src/components/splash/splash.jsx
+++ src/components/splash/splash.jsx
@@ -15,6 +15,7 @@ const Splash = () => {
             <p>To get started please search for a city of your choosing within the search box above.</p>
             <p>Should you find any bugs please don't be a stranger and post it in the issues board of my github, and it shall be fixed.</p>
             <p>Thank you for using the application!</p>
+            <p>     - Zahid Awan</p>
 
             <div className="socials">
                 <a className="social-link git" href="https://github.com/Zarhad-01/clima-view">
